59a48f96714fd165c2bf93eaeb7fde9a47f37d0d
Julia Werkoff <jwerkoff@gmail.com>
2015-02-01 22:19:30 +0100

    add test file

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include ("manager/BookManager.php");
+include ("model/Book.php");
+
+
+
+$bookManager = new BookManager();
+
+$bookManager->createBook("author", "title", "type", new \DateTime(), "status");
+$bookManager->createBook("Axel Venet", "L'Or de Rome", "Roman historique", new \DateTime(), "neuf");
+
+
