a7d4ed86e8e43514a35b11cd85b25c51f5e03625
Julia Werkoff <jwerkoff@gmail.com>
2015-02-02 23:02:18 +0100

    test function getBookList

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include ("model/Book.php");
 
 $bookManager = new BookManager();
 
-$bookManager->createBook("author", "title", "type", new \DateTime(), "status");
-$bookManager->createBook("Axel Venet", "L'Or de Rome", "Roman historique", new \DateTime(), "neuf");
+//$bookManager->createBook("author", "title", "type", new \DateTime(), "status");
+//$bookManager->createBook("Axel Venet", "L'Or de Rome", "Roman historique", new \DateTime(), "neuf");
 
-
+$bookManager->getBookList();
